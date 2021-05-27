b52fadf64cc58b6af9e4629d9fddb3026e3f0c64
Calais Galbraith <calaisgalbraith@gmail.com>
2021-05-27 11:47:16 -0500

    added codeup exercises java

--- js/external.js
+++ js/external.js
@@ -1,1 +1,10 @@
+"use strict";
+
+//... rest of javascript code
+
 console.log("Hello from Javascript");
+
+alert("Welcome to my Website!");
+
+var usercolor = prompt("What is your favorite color?")
+alert("Wow, " + usercolor + " is my favorite color too!")
