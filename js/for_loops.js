bc37b0d5aade24ce5ee86f9156d430dcce5d2dc8
Calais Galbraith <calaisgalbraith@gmail.com>
2021-06-04 09:58:21 -0500

    added exercise labels and line breaks to make my life easier lol

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,6 +10,8 @@ function showMultiplicationTable(input)
 }
 
 //exercise 3 // DONE
+console.log("Exercise 3");
+
 for(let x = 1; x < 11; x++)
 {
     var RandomNumber = Math.floor(Math.random() * (200-20) +20);
@@ -24,17 +26,23 @@ for(let x = 1; x < 11; x++)
 }
 
 //exercise 4: not looking that right?
+console.log("");
+console.log("Exercise 4")
+
+
 for(let i = 1; i < 10; i++)
 {
-    var x = 0;
-    while (x < i)
+    for (let x = 0; x < i; x++)
     {
         console.log(i);
-        x++;
     }
 }
 
+
 //exercise 5 // DONE
+console.log("");
+console.log("Exercise 5")
+
 for(var w = 100; w >= 5; w -= 5)
 {
     console.log(w);
