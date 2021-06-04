9763dd95c8ffdb3c604b868ddb2020d0e780a297
Calais Galbraith <calaisgalbraith@gmail.com>
2021-06-04 11:52:31 -0500

    fixed for loop exercise 4 with .repeat() s/o to Aarion

--- js/for_loops.js
+++ js/for_loops.js
@@ -25,11 +25,9 @@ console.log("");
 console.log("Exercise 4")
 
 for(let i = 1; i < 10; i++) {
-    for (let x = 0; x < i; x++) {
-        console.log(i);
-    }
+    var a = i.toString();
+    console.log(a.repeat(i))
 }
-
 
 //exercise 5 // DONE
 console.log("");
