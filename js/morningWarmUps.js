a8747cc18fdcfdcc804aa0cb7a6319572927221c
Calais Galbraith <calaisgalbraith@gmail.com>
2021-07-06 08:27:39 -0600

    morning exercises 7/6

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -19,3 +19,24 @@ function countSpaces(string){
 // DONE: Add validation to function above, if the argument pass is not a string it should return false.
     console.log(countSpaces("ch an el and Jub ilee"));
 
+
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+
+function capitalizeAllNames(array){
+    return array.toUpperCase();
+}
+
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+
+function capitalizeFirstLetter(array){
+    var tester = [];
+
+    for(let i = 0; i < array.length; i++){
+        tester[i] = array[i].charAt(0) + array[i].substr(1);
+    }
+    return tester;
+}
