20243e3ea20a752a842f9b2895fe4ef464e825c5
Calais Galbraith <calaisgalbraith@gmail.com>
2021-07-07 08:23:48 -0600

    morning exercises 7/7

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -9,7 +9,7 @@ function countSpaces(string){
 
     var spaces = 0;
     for (let i = 0; i < string.length; i++){
-        if (string.chatAt(i) === ""){
+        if (string.charAt(i) === " "){
             spaces++
         }
     }
@@ -42,7 +42,53 @@ function capitalizeFirstLetter(array){
     var tester = [];
 
     for(let i = 0; i < array.length; i++){
-        tester[i] = array[i].charAt(0) + array[i].substr(1);
+        tester[i] = array[i].charAt(0).toUpperCase() + array[i].substr(1);
     }
     return tester;
+}
+
+
+// July 7th 2021
+// DONE: Create a method on the object down below named getFullName that when called returns the users full name using the "this" key term.
+// Example: personOne.getFullName() ---- returns ----> "silvia floopertan"
+var personOne = {
+    firstName: "silvia",
+    lastName: "floopertan",
+    ageInYears: 34,
+    heightInInches: 65
+}
+
+    personOne.getFullName = function (){
+        return this.firstName + " " + this.lastName;
+    }
+
+
+// DONE: Create a method on the object down below named getNicelyFormattedFullName that when called returns the
+//  users full name with both first letters in the names being capitalized look at example down below for more reference.
+// Example: personOne.getNicelyFormattedFullName() ---- returns ----> "Dan Valdarez"
+var personTwo = {
+    firstName: "dan",
+    lastName: "valdarez",
+    ageInYears: 61,
+    heightInInches: 73
+}
+
+personTwo.getNicelyFormattedFullName = function (){
+    return this.firstName.charAt(0).toUpperCase() + this.firstName.substr(1) + " " + this.lastName.charAt(0).toUpperCase()
+            + this.lastName.substr(1);
+}
+
+// DONE: Create a method on the object below that gives us the behavior that the example implies.
+// Example: personThree.intro() ---- returns ----> "Hello, My name is Arlene Martinez and I am 25 years old"
+var personThree = {
+    firstName: "arlene",
+    lastName: "martinez",
+    ageInYears: 25,
+    heightInInches: 62
+}
+
+personThree.intro = function (){
+    return "Hello, my name is " + this.firstName.charAt(0).toUpperCase() +
+        this.firstName.substr(1)+ " " + this.lastName.charAt(0).toUpperCase()
+        + this.lastName.substr(1) + " and I am " + this.ageInYears + " years old"
 }
