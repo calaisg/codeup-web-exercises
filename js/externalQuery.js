101174c8c8684e687b512cbfcba35d8a3105440b
Calais Galbraith <calaisgalbraith@gmail.com>
2021-07-09 12:09:11 -0600

    finished mouse events exercises

--- js/externalQuery.js
+++ js/externalQuery.js
@@ -1,8 +1,35 @@
 "use strict"
 
 $(document).ready(function (){
-    //INTRO
-    //alert("DOM has finished loading");
+
+    //MOUSE EVENTS
+    $("h1").click(function (){
+        $(this).css("background-color", "yellow");
+    });
+
+    $("p").dblclick(function(){
+        $(this).css("font-size", "18px");
+    });
+
+    $("li").hover(
+        function (){
+            $(this).css("color", "red");
+        },
+        function(){
+            $(this).css("color", "black");
+        }
+    );
+
+    //ELEMENT SELECTORS
+    //$("li").css("font-size", "20px");
+    //$("h1, p, li").css("background-color", "yellow");
+
+    //var h1 = $("h1").html();
+    //alert(h1);
+
+    //CLASS SELECTORS
+    //create a border around all elements with the class codeup that is 1 pixel wide and red.
+    //$(".codeup").css("border", "1px solid #FF0000");
 
     //ID SELECTORS
     //var men = $("#men").html();
@@ -15,15 +42,7 @@ $(document).ready(function (){
     //var blake = $("#blake").html();
     //alert(blake);
 
-    //CLASS SELECTORS
-    //create a border around all elements with the class codeup that is 1 pixel wide and red.
-    //$(".codeup").css("border", "1px solid #FF0000");
-
-    //ELEMENT SELECTORS
-    $("li").css("font-size", "20px");
-    $("h1, p, li").css("background-color", "yellow");
-
-    var h1 = $("h1").html();
-    alert(h1);
+    //INTRO
+    //alert("DOM has finished loading");
 
 });
