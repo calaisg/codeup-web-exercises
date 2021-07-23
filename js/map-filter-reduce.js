41a4ae4f129de20f2c452801a164bdaaa35bcd04
Calais Galbraith <calaisgalbraith@gmail.com>
2021-07-23 10:04:42 -0500

    finished bonus for map-filter-reduce.js

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -42,52 +42,59 @@ const users = [
 // const threeLanguages = users.filter(function (n){
 //     return n.languages.length > 2;
 // })
-const threeLanguages = users.filter(n => n.languages.length > 2);
+        const threeLanguages = users.filter(n => n.languages.length > 2);
 
 //use .map to creat array of strings where each element is user's email address
 // const usersEmails = users.map(function (n){
 //     return n.email;
 // });
-const usersEmails = users.map(n => n.email);
+        const usersEmails = users.map(n => n.email);
 
-//use .reduce to get total years of experience from list users. Once get total, can calculate avg
-const totalYears = users.reduce((total, currentUser) => {
-    return total + currentUser.yearsOfExperience;
-}, 0);
+//use .reduce to get total years of experience from list users. Once get total, can calculate average
+        const totalYears = users.reduce((total, currentUser) => {
+            return total + currentUser.yearsOfExperience;
+        }, 0);
 
+        let averageYears = totalYears / users.length;
 
-
-
-
-//DONE: use .reduce to get longest email from list of users
-
-const longestEmail = users.reduce((currentLongestEmail, currentUser) => {
-    if (currentUser.email.length > currentLongestEmail.length){
-        return currentUser.email;
-    }
-    else{
-        return currentLongestEmail;
-    }
-    },"");
-
-// function findLongest(users){
-//     let returnEmail = ""
-//     const longestEmail = users.reduce((longest, currentUser) =>{
-//         if(currentUser.email.length > longest){
-//             longest = currentUser.email.length;
-//             returnEmail = currentUser.email;
-//         }
-//         return returnEmail;
-//     }, 0);
-//     return longestEmail;
-// };
-// console.log(findLongest(users));
+//use .reduce to get longest email from list of users
+        const longestEmail = users.reduce((currentLongestEmail, currentUser) => {
+            if (currentUser.email.length > currentLongestEmail.length){
+                return currentUser.email;
+            }
+            else{
+                return currentLongestEmail;
+            }
+            },"");
 
 //use .reduce to get lists of users name in single string
-//TODO: MAKE BETTER
-const listUsers = users.reduce((userList, currentUser) => {
-    return userList + currentUser.name;
-}, "");
+        const listUsers = users.reduce((userList, currentUser) => {
+           if(userList ===""){
+               return "Your instructors are: " + currentUser.name;
+           }
+           else{
+               return userList + ", " + currentUser.name;
+           }
+        }, "");
+
+        console.log(listUsers)
+
 //use .reduce to get unique list of languages from the list of users
+    let concatLanguages = users.reduce((languageList, currentUser) => {
+        return languageList.concat(currentUser.languages);
+    },"");
+
+
+    const uniqueLangauges = concatLanguages.reduce((languageList, currentUser) => {
+        if(languageList.includes(currentUser)){
+            return languageList;
+        }
+        else{
+            languageList.push(currentUser)
+            return languageList;
+        }
+    }, []);
+
+    console.log(uniqueLangauges);
 
 
