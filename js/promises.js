2949f86b27ca2bd75e2d7abad1d857f5d6031ed7
Calais Galbraith <calaisgalbraith@gmail.com>
2021-07-23 14:44:45 -0500

    finished promises.js

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,18 @@
 "use strict"
 
 
+//Create a function that accepts a GitHub username, and returns a promise that resolves
+// returning just the date of the last commit that user made.
+
+function lastCommit(username){
+    return fetch('https://api.github.com/users/' + username + '/events',{headers: {'Authorization': 'token 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'}} )
+        .then(response => response.json())
+        .then(user => console.log("Last push was: " + user[0].created_at));
+};
+
+//just return fetch request (no promise thing)
+lastCommit("calaisg");
+
 
 
 //Write a function named wait that accepts a number as a parameter, and returns a promise that
